9d82adcac7c5ee01cecb436f88697fb8cf509c15
Zeki <dev.zek@192.168.1.55>
2024-06-19 14:43:25 +0300

    tanstackquery added for manneging fetch

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { useQuery } from "@tanstack/react-query";
 
 const Main = () => {
+  const { data, error, isLoading } = useQuery("movies", () =>
+    fetch("https://jsonplaceholder.typicode.com/todos").then((res) =>
+      res.json(),
+    ),
+  );
+
   return <div>Main</div>;
 };
 
