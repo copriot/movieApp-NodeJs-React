1a74a843a0a2d16fa78c0786759d18f3f39fe153
Zeki <dev.zek@192.168.1.55>
2024-06-19 12:59:32 +0300

    get method prepared and tailwind added

--- backend/methods/get.js
+++ backend/methods/get.js
@@ -1,3 +1,41 @@
+const fs = require("fs");
+
 module.exports = async (req, res) => {
-    return res.end("Post isteginden gönderilen cevap");
+    // Yapılan isteğin temel adresi
+    const baseUrl = req.url.substring(0, req.url.lastIndexOf("/"));
+    // URL'in sonundaki id değerini bir değişkene aktar
+    const id = req.url.split("/")[3];
+
+    if (req.url === "/api/movies") {
+        // 1) Durum kodu belirle
+        res.statusCode = 200;
+
+        // 2) Header'ları belirle
+        res.setHeader("Content-Type", "application/json");
+
+        // 3) JSON dosyasından bütün filmleri al
+        const data = fs.readFileSync("./data/movies.json", "utf-8");
+
+        // 4) Client'a cevap gönder
+        return res.end(data);
+    } else if (baseUrl === "/api/movies" && id) {
+        // 1) Bütün filmleri al JS formatında
+        const data = JSON.parse(fs.readFileSync("./data/movies.json", "utf-8"));
+
+        // 2) URL'e eklenen id'ye karşılık gelen filmi dizide bul
+        const movie = data.movies.find((movie) => movie.id === id);
+
+        if (movie) {
+            // 3) Eğer film bulunursa client'a gönder
+            res.writeHead(200, { "Content-Type": "application/json" });
+            res.end(JSON.stringify(movie));
+        } else {
+            // 4) Film bulunamazsa client'a hata gönder
+            res.writeHead(404, { "Content-Type": "text/plain" });
+            res.end("Geçersiz id");
+        }
+    } else {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        return res.end("Yol bulunamadı");
+    }
 };
